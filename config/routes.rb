71ec09930cfbeb94d3df23eb254545a6f19a981a
shadmanAh <shadman061@gmail.com>
2022-01-03 19:02:23 +0330

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+
+  devise_for :users
+  resources :items do
+    member do
+      patch :complete
+    end
+  end
+  root 'items#index'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
